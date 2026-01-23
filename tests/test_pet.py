47a3685a22b0c521173026fc1118b38598878202
kis0n <l4d.kis0n@gmail.com>
2026-01-23 10:01:13 +0300

    Добавил тест "Попытка обновить несуществующего пользователя"

--- tests/test_pet.py
+++ tests/test_pet.py
@@ -8,11 +8,28 @@ class TestPet:
     @allure.title("Попытка удалить несуществующего питомца") #заголовок в алюр схожс заголовком тест-кейса
     def test_delete_nonexistent_pet(self): #название теста, функция пайтест
         with allure.step("Отправка запроса на удаление несуществующего питомца"): #описание шага в алюр
-            responce = requests.delete(url=f"{BASE_URL}/pet/9999")
-            print
+            response = requests.delete(url=f"{BASE_URL}/pet/9999")
 
         with allure.step("Проверка текста ответа"):
-            assert responce.text == 'Pet deleted', "Текст ответа не совпал с ожидаемым"
+            assert response.text == 'Pet deleted', "Текст ответа не совпал с ожидаемым"
 
         with allure.step("Проверка кода ответа"):
-            assert responce.status_code == 200, "Код ответа не совпал с ожидаемым"
+            assert response.status_code == 200, "Код ответа не совпал с ожидаемым"
+
+
+    @allure.title("Попытка обновить несуществующего пользователя")
+    def test_update_nonexistent_pet(self):
+        new_data = {
+            "id": 9999,
+            "name": "Non-existent Pet",
+            "status": "available"
+        }
+        with allure.step("Отправка запроса на обновление несуществующего питомца"):
+            response = requests.put(url=f"{BASE_URL}/pet", json=new_data)
+
+        with allure.step("Проверка кода ответа"):
+            assert response.status_code == 404, "Код ответа не совпал с ожидаемым"
+
+        with allure.step("Проверка текста ответа"):
+            assert response.text == 'Pet not found', "Текст ответа не совпал с ожидаемым"
+
